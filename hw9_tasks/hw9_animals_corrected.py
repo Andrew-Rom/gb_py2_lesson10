cf11855b18b14a5de844ba85b79bca0947ff6730
Andrew-Rom <1811mail@gmail.com>
2023-09-27 20:46:58 +0300

    Added hw9_animals_corrected.py

---
+++ hw9_tasks/hw9_animals_corrected.py
@@ -0,0 +1,51 @@
+class Animal:
+    def __init__(self, name, age, spec):
+        self.name = name
+        self.age = age
+        self.spec = spec
+
+    def get_spec(self):
+        return self.spec
+
+
+class Dog(Animal):
+    def __init__(self, name, age, spec):
+        super().__init__(name, age,spec)
+        self.spec = spec
+
+
+class Cat(Animal):
+    def __init__(self, name, age, spec):
+        super().__init__(name, age,spec)
+        self.spec = spec
+
+
+class Bird(Animal):
+    def __init__(self, name, age, spec):
+        super().__init__(name, age, spec)
+        self.spec = spec
+
+
+class AnimalShop:
+    @classmethod
+    def create(cls, animal_type, name, age, spec):
+        match animal_type.lower:
+            case 'dog':
+                animal = Dog(name, age, spec)
+            case 'cat':
+                animal = Cat(name, age, spec)
+            case 'bird':
+                animal = Bird(name, age, spec)
+            case _:
+                animal = Animal(name, age, spec)
+        return animal
+
+if __name__ == '__main__':
+    pet1 = AnimalShop.create('dog', 'Bob', 5, 'гавкает')
+    pet2 = AnimalShop.create('cat', 'Felix', 2, 'мурлычет')
+    pet3 = AnimalShop.create('bird', 'Aro', 1, 'поет')
+    pet4 = AnimalShop.create('fish', 'Pinky', 1, 'плавает')
+    pet5 = AnimalShop.create('pig', 'Boss', 1, 'орет')
+
+    for pet in [pet1, pet2, pet3, pet4, pet5]:
+        print(pet.name, pet.get_spec())
