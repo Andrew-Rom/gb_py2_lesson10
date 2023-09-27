e6f7f715ec01ec8fea99424f5acac727b4b6b750
Andrew-Rom <1811mail@gmail.com>
2023-09-27 19:13:50 +0300

    Corrected hw9_matrix

--- hw9_tasks/hw9_matrix.py
+++ hw9_tasks/hw9_matrix.py
@@ -1,30 +1,28 @@
 class Matrix():
     def __init__(self, data):
         self.data = data
+        self.is_matrix(self.data)
 
-    def isMatrix(self):
-        if type(self.data) is list:
-            for i in range(len(self.data)):
-                if type(self.data[i]) is not list:
-                    return False
+    def is_matrix(self, value):
+        if type(value) is list:
+            for i in range(len(value)):
+                if type(value) is not list:
+                    raise ValueError('This is not a matrix.')
             else:
-                return all(len(self.data[i]) == len(self.data[i + 1]) for i in range(len(self.data) - 1))
+                if not all(len(value[i]) == len(value[i + 1]) for i in range(len(value) - 1)):
+                    raise ValueError('This is not a matrix.')
         else:
-            return False
+            raise ValueError('This is not a matrix.')
 
     def transpose_matrix(self):
-        if self.isMatrix():
-            self.data = [[self.data[row][col] for row in range(len(self.data))] for col in range(len(self.data[0]))]
+        self.data = [[self.data[row][col] for row in range(len(self.data))] for col in range(len(self.data[0]))]
 
     def show_matrix(self):
-        if self.isMatrix():
-            print('\n'.join('\t'.join(map(str, row)) for row in self.data))
-            print()
-        else:
-            print('This is not matrix.')
+        print('\n'.join('\t'.join(map(str, row)) for row in self.data), '\n')
+
 
 if __name__ == '__main__':
-    my_matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    my_matrix.show_matrix()
-    my_matrix.transpose_matrix()
-    my_matrix.show_matrix()
+    matrix1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    matrix1.show_matrix()
+    matrix1.transpose_matrix()
+    matrix1.show_matrix()
