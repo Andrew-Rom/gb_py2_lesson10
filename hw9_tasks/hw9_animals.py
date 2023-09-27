5247844738f69d621c5f2b476329b9a5eed0f7ce
Andrew-Rom <1811mail@gmail.com>
2023-09-27 21:19:01 +0300

    Corrected hw9_animals

--- hw9_tasks/hw9_animals.py
+++ hw9_tasks/hw9_animals.py
@@ -1,4 +1,5 @@
 class Animal:
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -6,7 +7,6 @@ class Animal:
 
     def get_spec(self):
         return self.spec
-
 
 class Dog(Animal):
     def __init__(self, name, age, spec):
@@ -26,29 +26,28 @@ class Bird(Animal):
         self.spec = spec
 
 
-class CreateAnimal(Dog, Cat, Bird, Animal):
-
-    def __init__(self, animal, name, age, spec):
-        self.name = name
-        self.age = age
-        self.spec = spec
-        match animal.lower:
+class AnimalShop:
+    @classmethod
+    def create(cls, animal_type, name, age, spec):
+        match animal_type.lower():
             case 'dog':
-                self.animal = Dog(self.name, self.age, self.spec)
+                animal = Dog(name, age, spec)
             case 'cat':
-                self.animal = Cat(self.name, self.age, self.spec)
+                animal = Cat(name, age, spec)
             case 'bird':
-                self.animal = Bird(self.name, self.age, self.spec)
+                animal = Bird(name, age, spec)
             case _:
-                self.animal = Animal(self.name, self.age)
+                animal = Animal(name, age)
+                animal.spec = spec
+        return animal
 
 
 if __name__ == '__main__':
-    pet1 = CreateAnimal('Dog', 'Bob', 5, 'гавкает')
-    pet2 = CreateAnimal('Cat', 'Felix', 2, 'мурлычет')
-    pet3 = CreateAnimal('Bird', 'Aro', 1, 'поет')
-    pet4 = CreateAnimal('Fish', 'Pinky', 1, 'плавает')
-    pet5 = CreateAnimal('pig', 'Boss', 1, 'орет')
+    pet1 = AnimalShop.create('dog', 'Bob', 5, 'гавкает')
+    pet2 = AnimalShop.create('cat', 'Felix', 2, 'мурлычет')
+    pet3 = AnimalShop.create('bird', 'Aro', 1, 'поет')
+    pet4 = AnimalShop.create('fish', 'Pinky', 1, 'плавает')
+    pet5 = AnimalShop.create('pig', 'Boss', 1, 'орет')
 
     for pet in [pet1, pet2, pet3, pet4, pet5]:
         print(pet.name, pet.get_spec())
